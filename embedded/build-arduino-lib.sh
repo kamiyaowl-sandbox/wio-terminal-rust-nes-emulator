d32eafe8c80bc59208ad07ec0c6c9757633b4c1d
K <kamiyaowl@gmail.com>
2020-05-11 00:39:33 +0900

    まだだめ'

--- embedded/build-arduino-lib.sh
+++ embedded/build-arduino-lib.sh
@@ -9,6 +9,6 @@ rustup run nightly cargo build --release
 mkdir -p "rust_nes_emulator_embedded/src/cortex-m4"
 mkdir -p "rust_nes_emulator_embedded/src/cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard"
 cp "rust_nes_emulator_embedded.h" "rust_nes_emulator_embedded/src/rust_nes_emulator_embedded.h"
-cp "target/thumbv7em-none-eabihf/release/librust_nes_emulator_embedded.a" "rust_nes_emulator_embedded/src/cortex-m4/rust_nes_emulator_embedded.a"
-cp "target/thumbv7em-none-eabihf/release/librust_nes_emulator_embedded.a" "rust_nes_emulator_embedded/src/cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard/rust_nes_emulator_embedded.a"
+cp "target/thumbv7em-none-eabihf/release/librust_nes_emulator_embedded.a" "rust_nes_emulator_embedded/src/cortex-m4/librust_nes_emulator_embedded.a"
+cp "target/thumbv7em-none-eabihf/release/librust_nes_emulator_embedded.a" "rust_nes_emulator_embedded/src/cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard/librust_nes_emulator_embedded.a"
 zip -r rust_nes_emulator_embedded.zip rust_nes_emulator_embedded
